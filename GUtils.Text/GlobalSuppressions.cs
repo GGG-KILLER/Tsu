56fe5d919789c2a4cd0993882dcd0083035ebd8a
GGG KILLER <gggkiller2@gmail.com>
2018-05-18 03:52:04 -0300

    Removed useless GUtils.Text

--- GUtils.Text/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project. Project-level
-// suppressions either have no target or are given a specific
-// target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "IDE0008:Use explicit type", Justification = "<Pending>", Scope = "member", Target = "~M:GUtils.Text.TReader`1.ReadT(System.Int32)~`0[]" )]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "IDE0008:Use explicit type", Justification = "<Pending>", Scope = "member", Target = "~M:GUtils.Text.TReader`1.ReadSeq(System.Int32)~`0[]" )]
