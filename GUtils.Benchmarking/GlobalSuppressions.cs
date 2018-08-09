d50b618a5312456b9cdfa8022fd1d01132a4a614
GGG KILLER <gggkiller2@gmail.com>
2018-08-08 21:52:01 -0300

    Remove """benchmarking""" lib

--- GUtils.Benchmarking/GlobalSuppressions.cs
+++
@@ -1,6 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project. Project-level
-// suppressions either have no target or are given a specific
-// target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "IDE0008:Use explicit type", Justification = "Type is already explicit.", Scope = "member", Target = "~M:GUtils.Benchmarking.Benchmark.Compare(System.Action[])~GUtils.Benchmarking.BenchmarkComparisonResult[]" )]
