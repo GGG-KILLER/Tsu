3619b766e765f2132f86d92f4f01ec9900c8c1a2
GGG KILLER <gggkiller2@gmail.com>
2018-08-08 21:52:37 -0300

    Disable some warnings/infos
    TODO: Give a reason to all of them

---
+++ GUtils.Net/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project. Project-level
+// suppressions either have no target or are given a specific
+// target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Usage", "CC0074:Make field readonly", Justification = "<Pending>", Scope = "member", Target = "~F:GUtils.Net.DownloadClient.UserAgent" )]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "IDE0007:Use implicit type", Justification = "<Pending>", Scope = "member", Target = "~M:GUtils.Net.DownloadClient.DownloadToStreamAsync(System.IO.Stream,System.Int32)~System.Threading.Tasks.Task" )]
--- GUtils.Windows.Forms/GlobalSuppressions.cs
+++ GUtils.Windows.Forms/GlobalSuppressions.cs
@@ -4,3 +4,7 @@
 // target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "IDE0008:Use explicit type", Justification = "<Pending>", Scope = "member", Target = "~M:GUtils.Windows.Forms.FolderSelectDialog.ShowDialog(System.Windows.Forms.IWin32Window)~System.Boolean" )]
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Usage", "CC0074:Make field readonly", Justification = "<Pending>", Scope = "member", Target = "~F:GUtils.Windows.Forms.ToolTipManager.ToolTipInfo.Icon" )]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Usage", "CC0074:Make field readonly", Justification = "<Pending>", Scope = "member", Target = "~F:GUtils.Windows.Forms.ToolTipManager.ToolTipInfo.Message" )]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Usage", "CC0074:Make field readonly", Justification = "<Pending>", Scope = "member", Target = "~F:GUtils.Windows.Forms.ToolTipManager.ToolTipInfo.Title" )]
