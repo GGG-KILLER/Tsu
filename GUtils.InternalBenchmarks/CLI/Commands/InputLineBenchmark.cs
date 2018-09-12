82411e003caf86712435434c380d72ae4d18cd5d
GGG KILLER <gggkiller2@gmail.com>
2018-09-12 13:45:10 -0300

    Benchmarks for internal decision making

---
+++ GUtils.InternalBenchmarks/CLI/Commands/InputLineBenchmark.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using GUtils.CLI.Commands;
+
+namespace GUtils.InternalBenchmarks.CLI.Commands
+{
+    [ClrJob, CoreJob ( true ), DryCoreJob, DryClrJob]
+    public class InputLineBenchmark
+    {
+        [Benchmark ( Description = "Simple parsing" )]
+        [Arguments ( @"some long input ""string for the"" parser" )]
+        public String[] SimpleParse ( String input )
+        {
+            return InputLineParser.SimpleParse ( input ).ToArray ( );
+        }
+
+        [Benchmark ( Description = "Complex parsing")]
+        [Arguments ( @"let\'s ""\o163\b1110100\x61\o162\o164 \o142\o171\x20\o163\97y\105\b1101110\x67"" r:\o150\b1100101\o154\x6c\x6f\x20\b1110111\o157\x72\o154\x64 how are you?" )]
+        public String[] ComplexParse ( String input )
+        {
+            return InputLineParser.Parse ( input ).ToArray ( );
+        }
+    }
+}
---
+++ GUtils.InternalBenchmarks/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace GUtils.InternalBenchmarks
+{
+    class Program
+    {
+        static void Main ( String[] args )
+        {
+            BenchmarkSwitcher.FromAssembly ( typeof ( Program ).Assembly )
+                .Run ( args, DefaultConfig.Instance.With ( Encoding.UTF8 ).KeepBenchmarkFiles ( false ) );
+        }
+    }
+}
