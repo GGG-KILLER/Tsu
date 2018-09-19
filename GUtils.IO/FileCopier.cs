a1d8e9614718c78b878d62daef48b6c4ca07005c
GGG KILLER <gggkiller2@gmail.com>
2018-09-18 23:53:22 -0300

    Coding style updates

--- GUtils.IO/FileCopier.cs
+++ GUtils.IO/FileCopier.cs
@@ -16,20 +16,6 @@
  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-/*
- * Copyright © 2016 GGG KILLER <gggkiller2@gmail.com>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”),
- * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- */
 
 namespace GUtils.IO
 {
@@ -90,8 +76,8 @@ namespace GUtils.IO
             if ( ti.Exists )
                 ti.Delete ( );
             ti.Directory.Create ( );
-            using ( var reader = File.OpenRead ( fi.FullName ) )
-            using ( var writer = File.OpenWrite ( ti.FullName ) )
+            using ( FileStream reader = File.OpenRead ( fi.FullName ) )
+            using ( FileStream writer = File.OpenWrite ( ti.FullName ) )
             {
                 var lastPos = 0L;
                 while ( true )
--- GUtils.IO/NullStream.cs
+++ GUtils.IO/NullStream.cs
@@ -16,6 +16,7 @@
  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 using System;
 using System.IO;
 using System.Threading;
@@ -56,71 +57,42 @@ namespace GUtils.IO
             set => base.WriteTimeout = value;
         }
 
-        public override Task CopyToAsync ( Stream destination, Int32 bufferSize, CancellationToken cancellationToken )
-        {
-            throw new NotSupportedException ( "This stream does not support reading." );
-        }
+        public override Task CopyToAsync ( Stream destination, Int32 bufferSize, CancellationToken cancellationToken ) => throw new NotSupportedException ( "This stream does not support reading." );
 
-        public override Boolean Equals ( Object obj )
-        {
-            return obj is NullStream;
-        }
+        public override Boolean Equals ( Object obj ) => obj is NullStream;
 
         public override void Flush ( )
         {
             // Do nothing
         }
 
-        public override Task FlushAsync ( CancellationToken cancellationToken )
-        {
-            return Task.CompletedTask;
-        }
+        public override Task FlushAsync ( CancellationToken cancellationToken ) => Task.CompletedTask;
 
-        public override Int32 GetHashCode ( )
-        {
+        public override Int32 GetHashCode ( ) =>
             // All instances of this are one and the same
-            return 0;
-        }
+            0;
 
-        public override Int32 Read ( Byte[] buffer, Int32 offset, Int32 count )
-        {
-            throw new NotSupportedException ( "This stream does not support reading." );
-        }
+        public override Int32 Read ( Byte[] buffer, Int32 offset, Int32 count ) => throw new NotSupportedException ( "This stream does not support reading." );
 
-        public override Task<Int32> ReadAsync ( Byte[] buffer, Int32 offset, Int32 count, CancellationToken cancellationToken )
-        {
-            throw new NotSupportedException ( "This stream does not support reading." );
-        }
+        public override Task<Int32> ReadAsync ( Byte[] buffer, Int32 offset, Int32 count, CancellationToken cancellationToken ) => throw new NotSupportedException ( "This stream does not support reading." );
 
-        public override Int32 ReadByte ( )
-        {
-            throw new NotSupportedException ( "This stream does not support reading." );
-        }
+        public override Int32 ReadByte ( ) => throw new NotSupportedException ( "This stream does not support reading." );
 
-        public override Int64 Seek ( Int64 offset, SeekOrigin origin )
-        {
-            throw new NotSupportedException ( "This stream does not support seeking." );
-        }
+        public override Int64 Seek ( Int64 offset, SeekOrigin origin ) => throw new NotSupportedException ( "This stream does not support seeking." );
 
         public override void SetLength ( Int64 value )
         {
             // Nothing
         }
 
-        public override String ToString ( )
-        {
-            return "/dev/null stream";
-        }
+        public override String ToString ( ) => "/dev/null stream";
 
         public override void Write ( Byte[] buffer, Int32 offset, Int32 count )
         {
             // Do nothing either
         }
 
-        public override Task WriteAsync ( Byte[] buffer, Int32 offset, Int32 count, CancellationToken cancellationToken )
-        {
-            return Task.CompletedTask;
-        }
+        public override Task WriteAsync ( Byte[] buffer, Int32 offset, Int32 count, CancellationToken cancellationToken ) => Task.CompletedTask;
 
         public override void WriteByte ( Byte value )
         {
