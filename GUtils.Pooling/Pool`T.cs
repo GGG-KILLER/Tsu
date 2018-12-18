8f6d763a6885b4914cf2521e3025255f94b3f0ef
GGG KILLER <gggkiller2@gmail.com>
2018-12-18 06:05:34 -0200

    Add pooling library

---
+++ GUtils.Pooling/Pool`T.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace GUtils.Pooling
+{
+    /// <summary>
+    /// Represents a pool of objects of type
+    /// <typeparamref name="T" />. Used to reduce the amount of
+    /// allocations throughout the program.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <remarks>
+    /// Restricted to classes because value types are passed by
+    /// copy, thus there's no point to having them in a pool.
+    /// </remarks>
+    public abstract partial class Pool<T> where T : class
+    {
+        /// <summary>
+        /// The bucket containing the pooled resources
+        /// </summary>
+        private readonly ConcurrentBag<T> Bucket;
+
+        /// <summary>
+        /// Whether returned items should be cleared by default
+        /// </summary>
+        public Boolean AreItemsClearedByDefault { get; }
+
+        /// <summary>
+        /// The maximum capacity of this <see cref="Pool{T}" />
+        /// </summary>
+        public Int32 Capacity { get; }
+
+        /// <summary>
+        /// Initializes the pool with the given capacity and sets
+        /// the default returned item clearing behavior.
+        /// </summary>
+        /// <param name="capacity"></param>
+        /// <param name="shouldClearByDefault"></param>
+        protected Pool ( Int32 capacity, Boolean shouldClearByDefault )
+        {
+            this.Bucket = new ConcurrentBag<T> ( );
+            this.AreItemsClearedByDefault = shouldClearByDefault;
+            this.Capacity = capacity;
+        }
+
+        /// <summary>
+        /// Initializes a new item
+        /// </summary>
+        /// <returns></returns>
+        protected abstract T InitializeItem ( );
+
+        /// <summary>
+        /// Resets an item's state
+        /// </summary>
+        /// <param name="item"></param>
+        protected abstract void ResetItem ( T item );
+
+        /// <summary>
+        /// Rents an object.
+        /// </summary>
+        /// <returns></returns>
+        public T Rent ( ) =>
+
+            // Return an object if we have one stored otherwise
+            // create and return a new one
+            this.Bucket.TryTake ( out T item )
+                ? item
+                : this.InitializeItem ( );
+
+        /// <summary>
+        /// Returns to the pool an item obtained with
+        /// <see cref="Rent" />.
+        /// </summary>
+        /// <param name="item"></param>
+        public void Return ( T item ) => this.Return ( item, this.AreItemsClearedByDefault );
+
+        /// <summary>
+        /// Returns to the pool an item obtained with
+        /// <see cref="Rent" />
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="resetItem"></param>
+        public void Return ( T item, Boolean resetItem )
+        {
+            // If we're already over the capacity, just throw the
+            // object away.
+            if ( this.Bucket.Count >= this.Capacity )
+                return;
+
+            // Otherwise check and apply the default clearing
+            // behavior
+            if ( resetItem )
+                this.ResetItem ( item );
+
+            // And add the object back into the bucket
+            this.Bucket.Add ( item );
+        }
+
+        /// <summary>
+        /// Executes an action with a rented item
+        /// </summary>
+        /// <param name="action"></param>
+        public void WithRentedItem ( Action<T> action )
+        {
+            if ( action == null )
+                throw new ArgumentNullException ( nameof ( action ) );
+
+            T item = this.Rent ( );
+            try
+            {
+                action ( item );
+            }
+            finally
+            {
+                this.Return ( item );
+            }
+        }
+
+        /// <summary>
+        /// Executes a function with a rented item
+        /// </summary>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public R WithRentedItem<R> ( Func<T, R> func )
+        {
+            if ( func == null )
+                throw new ArgumentNullException ( nameof ( func ) );
+
+            T item = this.Rent ( );
+            try
+            {
+                return func ( item );
+            }
+            finally
+            {
+                this.Return ( item );
+            }
+        }
+    }
+}
---
+++ GUtils.Pooling/StringBuilderPool.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace GUtils.Pooling
+{
+    /// <summary>
+    /// A <see cref="Pool{T}" /> of <see cref="StringBuilder" />
+    /// </summary>
+    public class StringBuilderPool : Pool<StringBuilder>
+    {
+        /// <summary>
+        /// A shared pool
+        /// </summary>
+        public static readonly StringBuilderPool Shared = new StringBuilderPool ( 50 );
+
+        /// <summary>
+        /// Initializes a new <see cref="StringBuilder"/> <see cref="Pool{T}"/> with a custom <paramref name="capacity"/>
+        /// </summary>
+        /// <param name="capacity"></param>
+        public StringBuilderPool ( Int32 capacity ) : base ( capacity, true )
+        {
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <returns></returns>
+        protected override StringBuilder InitializeItem ( ) => new StringBuilder ( );
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <param name="item"></param>
+        protected override void ResetItem ( StringBuilder item ) => item.Clear ( );
+    }
+}
