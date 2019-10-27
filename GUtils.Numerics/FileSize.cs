10cd43d67b41b40ea6a05b491b4476ca66408286
GGG KILLER <gggkiller2@gmail.com>
2019-10-27 13:22:48 -0300

    Inclusion of GUtils.Numerics

---
+++ GUtils.Numerics/FileSize.cs
@@ -0,0 +1,131 @@
+/*
+ * Copyright © 2019 GGG KILLER <gggkiller2@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the “Software”), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+using System;
+
+namespace GUtils.Numerics
+{
+    /// <summary>
+    /// An utility to deal with file sizes
+    /// </summary>
+    public static class FileSize
+    {
+        /// <summary>
+        /// A KiB in bytes
+        /// </summary>
+        public const Int32 KiB = 1 << 10;
+
+        /// <summary>
+        /// A MiB in bytes
+        /// </summary>
+        public const Int32 MiB = 1 << 20;
+
+        /// <summary>
+        /// A GiB in bytes
+        /// </summary>
+        public const Int32 GiB = 1 << 30;
+
+        /// <summary>
+        /// A TiB in bytes
+        /// </summary>
+        public const Int64 TiB = 1 << 40;
+
+        /// <summary>
+        /// A PiB in bytes
+        /// </summary>
+        public const Int64 PiB = 1 << 50;
+
+        /// <summary>
+        /// An EiB in bytes
+        /// </summary>
+        public const Int64 EiB = 1 << 60;
+
+        private static readonly String[] _suffixes = { "B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB" };
+
+        /// <summary>
+        /// Returns the size provided as a pair of the new size and the suffix
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static (Double, String) GetFormatPair ( Int64 size )
+        {
+            if ( size == 0 ) return (size, "B");
+            var i = ( Int32 ) Math.Floor ( Math.Log ( Math.Abs ( size ), 1024 ) );
+            return (size / Math.Pow ( 1024, i ), _suffixes[i]);
+        }
+
+        /// <summary>
+        /// Returns the size provided as a pair of the new size and the suffix
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static (Double, String) GetFormatPair ( Double size )
+        {
+            if ( Double.IsInfinity ( size ) || Double.IsNaN ( size ) || size == 0D || size == -0D )
+                return (size, "B");
+            var i = ( Int32 ) Math.Floor ( Math.Log ( Math.Abs ( size ), 1024 ) );
+            return (size / Math.Pow ( 1024, i ), _suffixes[i]);
+        }
+
+        /// <summary>
+        /// Formats the provided file size in a human readable format (in bibytes)
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static String Format ( Int64 size )
+        {
+            (var newSize, var suffix) = GetFormatPair ( size );
+            return $"{newSize:0.##} {suffix}";
+        }
+
+        /// <summary>
+        /// Formats the provided file size in a human readable format (in bibytes)
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static String Format ( Double size )
+        {
+            (var newSize, var suffix) = GetFormatPair ( size );
+            return $"{newSize:0.##} {suffix}";
+        }
+
+        /// <summary>
+        /// Formats the provided file size in a human readable format (in bibytes)
+        /// </summary>
+        /// <param name="size"></param>
+        /// <param name="format">default value is: "{0} {1}"</param>
+        /// <returns></returns>
+        public static String Format ( Int64 size, String format )
+        {
+            (var newSize, var suffix) = GetFormatPair ( size );
+            return String.Format ( format, newSize, suffix );
+        }
+
+        /// <summary>
+        /// Formats the provided file size in a human readable format (in bibytes)
+        /// </summary>
+        /// <param name="size"></param>
+        /// <param name="format">default value is: "{0} {1}"</param>
+        /// <returns></returns>
+        public static String Format ( Double size, String format )
+        {
+            (var newSize, var suffix) = GetFormatPair ( size );
+            return String.Format ( format, newSize, suffix );
+        }
+    }
+}
---
+++ GUtils.Numerics/SI.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace GUtils.Numerics
+{
+    /// <summary>
+    /// A subset of the International System of Units Prefixes that only contains the prefixes that are
+    /// powers of 1000
+    /// </summary>
+    public static class SI
+    {
+        /// <summary>
+        /// A yotta (Y)
+        /// </summary>
+        public const Double Yotta = Zetta * 1000;
+
+        /// <summary>
+        /// A zetta (Z)
+        /// </summary>
+        public const Double Zetta = Exa   * 1000;
+
+        /// <summary>
+        /// An exa (E)
+        /// </summary>
+        public const Double Exa   = Peta  * 1000;
+
+        /// <summary>
+        /// A peta (P)
+        /// </summary>
+        public const Double Peta  = Tera  * 1000;
+
+        /// <summary>
+        /// A tera (T)
+        /// </summary>
+        public const Double Tera  = Giga  * 1000;
+
+        /// <summary>
+        /// A giga (G)
+        /// </summary>
+        public const Double Giga  = Mega  * 1000;
+
+        /// <summary>
+        /// A mega (M)
+        /// </summary>
+        public const Double Mega  = Kilo  * 1000;
+
+        /// <summary>
+        /// A kilo (K)
+        /// </summary>
+        public const Double Kilo  = 1000;
+
+        /// <summary>
+        /// A milli (m)
+        /// </summary>
+        public const Double Milli = 0.001;
+
+        /// <summary>
+        /// A micro (μ)
+        /// </summary>
+        public const Double Micro = Milli * 0.001;
+
+        /// <summary>
+        /// A nano (n)
+        /// </summary>
+        public const Double Nano  = Micro * 0.001;
+
+        /// <summary>
+        /// A pico (p)
+        /// </summary>
+        public const Double Pico  = Nano  * 0.001;
+
+        /// <summary>
+        /// A femto (f)
+        /// </summary>
+        public const Double Femto = Pico  * 0.001;
+
+        /// <summary>
+        /// An atto (a)
+        /// </summary>
+        public const Double Atto  = Femto * 0.001;
+
+        /// <summary>
+        /// A zepto (z)
+        /// </summary>
+        public const Double Zepto = Atto  * 0.001;
+
+        /// <summary>
+        /// A yocto (y)
+        /// </summary>
+        public const Double Yocto = Zepto * 0.001;
+
+        private static readonly Char[] _prefixes =
+        {
+            /* -8 */ 'y',
+            /* -7 */ 'z',
+            /* -6 */ 'a',
+            /* -5 */ 'f',
+            /* -4 */ 'p',
+            /* -3 */ 'n',
+            /* -2 */ 'μ',
+            /* -1 */ 'm',
+            /*  0 */ '\uFFFF',
+            /*  1 */ 'k',
+            /*  2 */ 'M',
+            /*  3 */ 'G',
+            /*  4 */ 'T',
+            /*  5 */ 'P',
+            /*  6 */ 'E',
+            /*  7 */ 'Z',
+            /*  8 */ 'Y',
+        };
+
+        /// <summary>
+        /// Returns the pair of objects used in suffixing a SI number
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static (Double, String) GetFormatPair ( Double number )
+        {
+            var pow = ( Int32 ) Math.Max ( Math.Min ( Math.Log ( number, 1000d ), 8 ), -8 );
+            if ( pow == 0 )
+                return (number, "");
+
+            return (number / Math.Pow ( 1000, pow ), _prefixes[8 + pow].ToString ( ));
+        }
+
+        /// <summary>
+        /// Formats a <paramref name="number" /> with its SI suffix according to the
+        /// <paramref name="format" />
+        /// </summary>
+        /// <param name="number">The number to be formatted</param>
+        /// <param name="format">The format string. Must contain two format locations/slots.</param>
+        /// <returns></returns>
+        public static String Format ( Double number, String format )
+        {
+            (var scaled, var suffix) = GetFormatPair ( number );
+            return String.Format ( format, scaled, suffix );
+        }
+
+        /// <summary>
+        /// Formats a <paramref name="number" /> with it's SI prefix appended to it
+        /// </summary>
+        /// <param name="number">The number to format</param>
+        /// <returns></returns>
+        public static String Format ( Double number ) =>
+            Format ( number, "{0} {1}" );
+    }
+}
