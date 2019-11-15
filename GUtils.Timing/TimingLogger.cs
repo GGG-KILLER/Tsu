a4a0e2054337b877d49549e9d28fee376c4b0902
GGG KILLER <gggkiller2@gmail.com>
2019-11-15 16:33:25 -0300

    Improve timing logger

--- GUtils.Timing/TimingLogger.cs
+++ GUtils.Timing/TimingLogger.cs
@@ -16,6 +16,7 @@
  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -64,29 +65,24 @@ namespace GUtils.Timing
         private class Scope : IDisposable
         {
             /// <summary>
-            /// The <see cref="TimingLogger" /> that owns this scope
+            /// The <see cref="TimingLogger"/> that owns this scope
             /// </summary>
-            private readonly TimingLogger Owner;
-
-            /// <summary>
-            /// Boxed parent
-            /// </summary>
-            private readonly Scope Parent;
+            private readonly TimingLogger _owner;
 
             /// <summary>
             /// The user provided name of the scope
             /// </summary>
-            public readonly String Name;
+            public String Name { get; }
 
             /// <summary>
-            /// The <see cref="Elapsed" /> at which this scope was entered
+            /// The <see cref="Elapsed"/> at which this scope was entered
             /// </summary>
-            public readonly TimeSpan StartedAt;
+            public TimeSpan StartedAt { get; }
 
             /// <summary>
             /// Whether the amount of time elapsed on this scope should be printed at the end.
             /// </summary>
-            public readonly Boolean ShouldPrintElapsedTime;
+            public Boolean ShouldPrintElapsedTime { get; }
 
             /// <summary>
             /// Initializes a scope
@@ -96,22 +92,13 @@ namespace GUtils.Timing
             /// <param name="startedAt"></param>
             /// <param name="shouldPrintElapsedTime"></param>
             /// <param name="parent"></param>
-            public Scope ( TimingLogger owner, Scope parent, String name, TimeSpan startedAt, Boolean shouldPrintElapsedTime )
+            public Scope ( TimingLogger owner, String name, TimeSpan startedAt, Boolean shouldPrintElapsedTime )
             {
-                this.Owner = owner;
-                this.Parent = parent;
+                this._owner = owner;
                 this.Name = name;
                 this.StartedAt = startedAt;
                 this.ShouldPrintElapsedTime = shouldPrintElapsedTime;
             }
-
-            /// <summary>
-            /// Whether the a given <paramref name="scope" /> is the parent of this scope
-            /// </summary>
-            /// <param name="scope"></param>
-            /// <returns></returns>
-            public Boolean IsInTree ( Scope scope ) =>
-                this.Equals ( scope ) || ( this.Parent is Scope parent && ( parent.Equals ( scope ) || parent.IsInTree ( parent ) ) );
 
             private Boolean disposed;
 
@@ -119,7 +106,56 @@ namespace GUtils.Timing
             {
                 if ( !this.disposed )
                 {
-                    this.Owner.EndScope ( this );
+                    this._owner.EndScope ( this );
+                    this.disposed = true;
+                }
+            }
+        }
+
+        private class Operation : IDisposable
+        {
+            /// <summary>
+            /// The <see cref="TimingLogger"/> that owns this scope
+            /// </summary>
+            private readonly TimingLogger _owner;
+
+            /// <summary>
+            /// The user provided name of the scope
+            /// </summary>
+            public String Name { get; }
+
+            /// <summary>
+            /// The <see cref="Elapsed"/> at which this scope was entered
+            /// </summary>
+            public TimeSpan StartedAt { get; }
+
+            /// <summary>
+            /// Whether the amount of time elapsed on this scope should be printed at the end.
+            /// </summary>
+            public Boolean ShouldPrintElapsedTime { get; }
+
+            /// <summary>
+            /// Initializes a scope
+            /// </summary>
+            /// <param name="owner"></param>
+            /// <param name="name"></param>
+            /// <param name="startedAt"></param>
+            /// <param name="shouldPrintElapsedTime"></param>
+            public Operation ( TimingLogger owner, String name, TimeSpan startedAt, Boolean shouldPrintElapsedTime )
+            {
+                this._owner = owner;
+                this.Name = name;
+                this.StartedAt = startedAt;
+                this.ShouldPrintElapsedTime = shouldPrintElapsedTime;
+            }
+
+            private Boolean disposed;
+
+            void IDisposable.Dispose ( )
+            {
+                if ( !this.disposed )
+                {
+                    this._owner.EndScope ( this );
                     this.disposed = true;
                 }
             }
@@ -136,22 +172,22 @@ namespace GUtils.Timing
         #region Logging Level Colors
 
         /// <summary>
-        /// Color used for the prefix of <see cref="LogLevel.Debug" /> messages
+        /// Color used for the prefix of <see cref="LogLevel.Debug"/> messages
         /// </summary>
         protected ConsoleColor DebugColor { get; set; } = ConsoleColor.DarkGray;
 
         /// <summary>
-        /// Color used for the prefix of <see cref="LogLevel.Information" /> messages
+        /// Color used for the prefix of <see cref="LogLevel.Information"/> messages
         /// </summary>
         protected ConsoleColor InformationColor { get; set; } = ConsoleColor.DarkBlue;
 
         /// <summary>
-        /// Color used for the prefix of <see cref="LogLevel.Warning" /> messages
+        /// Color used for the prefix of <see cref="LogLevel.Warning"/> messages
         /// </summary>
         protected ConsoleColor WarningColor { get; set; } = ConsoleColor.Yellow;
 
         /// <summary>
-        /// Color used for the prefix of <see cref="LogLevel.Error" /> messages
+        /// Color used for the prefix of <see cref="LogLevel.Error"/> messages
         /// </summary>
         protected ConsoleColor ErrorColor { get; set; } = ConsoleColor.Red;
 
@@ -165,21 +201,21 @@ namespace GUtils.Timing
         /// <summary>
         /// Whether to pring
         /// <code>
-        /// [INFO]
+        ///[INFO]
         /// </code>
         /// ,
         /// <code>
-        /// [DBUG]
+        ///[DBUG]
         /// </code>
         /// ,
         /// <code>
-        /// [WARN]
+        ///[WARN]
         /// </code>
         /// and
         /// <code>
-        /// [FAIL]
+        ///[FAIL]
         /// </code>
-        /// prefixes according to the message's <see cref="LogLevel" />
+        /// prefixes according to the message's <see cref="LogLevel"/>
         /// </summary>
         public Boolean PrintLevelPrefixes { get; set; }
 
@@ -232,8 +268,7 @@ namespace GUtils.Timing
         #region Input message processing
 
         /// <summary>
-        /// Writes the elapsed time and the <see cref="LogLevel" /> prefix, if enabled through
-        /// <see cref="PrintLevelPrefixes" />
+        /// Writes the elapsed time and the <see cref="LogLevel"/> prefix, if enabled through <see cref="PrintLevelPrefixes"/>
         /// </summary>
         /// <param name="level"></param>
         protected virtual void WriteLinePrefix ( LogLevel level )
@@ -303,7 +338,12 @@ namespace GUtils.Timing
             }
         }
 
-        private void ProcessWriteLine ( LogLevel level, String message )
+        /// <summary>
+        /// Processes a WriteLine request with a specified log level and message
+        /// </summary>
+        /// <param name="level"></param>
+        /// <param name="message"></param>
+        protected virtual void ProcessWriteLine ( LogLevel level, String message )
         {
             this.ProcessWrite ( level, message );
             this.WriteLineInternal ( "" );
@@ -319,20 +359,21 @@ namespace GUtils.Timing
         /// </summary>
         /// <param name="value"></param>
         public void Write ( Object value ) =>
-            this.ProcessWrite ( LogLevel.None, value.ToString ( ) );
+            this.ProcessWrite ( LogLevel.None, value?.ToString ( ) );
 
         /// <summary>
         /// Writes a value to the output
         /// </summary>
         /// <param name="value"></param>
         public void Write ( String value ) =>
-            this.ProcessWrite ( LogLevel.None, value.ToString ( ) );
+            this.ProcessWrite ( LogLevel.None, value );
 
         /// <summary>
         /// Writes a value to the output
         /// </summary>
         /// <param name="format"></param>
         /// <param name="args"></param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage ( "Globalization", "CA1305:Specify IFormatProvider", Justification = "As designed." )]
         public void Write ( String format, params Object[] args ) =>
             this.ProcessWrite ( LogLevel.None, String.Format ( format, args ) );
 
@@ -341,20 +382,21 @@ namespace GUtils.Timing
         /// </summary>
         /// <param name="value"></param>
         public void WriteLine ( Object value ) =>
-            this.ProcessWriteLine ( LogLevel.None, value.ToString ( ) );
+            this.ProcessWriteLine ( LogLevel.None, value?.ToString ( ) );
 
         /// <summary>
         /// Writes a value to the output followed by a new line
         /// </summary>
         /// <param name="value"></param>
         public void WriteLine ( String value ) =>
-            this.ProcessWriteLine ( LogLevel.None, value.ToString ( ) );
+            this.ProcessWriteLine ( LogLevel.None, value );
 
         /// <summary>
         /// Writes a value to the output followed by a new line
         /// </summary>
         /// <param name="format"></param>
         /// <param name="args"></param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage ( "Globalization", "CA1305:Specify IFormatProvider", Justification = "As designed." )]
         public void WriteLine ( String format, params Object[] args ) =>
             this.ProcessWriteLine ( LogLevel.None, String.Format ( format, args ) );
 
@@ -366,15 +408,36 @@ namespace GUtils.Timing
         /// Logs a message to the output
         /// </summary>
         /// <param name="level"></param>
+        /// <param name="message"></param>
+        public virtual void LogMessage ( LogLevel level, String message )
+        {
+            if ( level < this.MinimumLogLevel )
+                return;
+
+            this.ProcessWriteLine ( level, message );
+        }
+
+        /// <summary>
+        /// Logs a message to the output
+        /// </summary>
+        /// <param name="level"></param>
         /// <param name="format"></param>
         /// <param name="args"></param>
-        public void LogMessage ( LogLevel level, String format, params Object[] args )
+        [System.Diagnostics.CodeAnalysis.SuppressMessage ( "Globalization", "CA1305:Specify IFormatProvider", Justification = "As designed." )]
+        public virtual void LogMessage ( LogLevel level, String format, params Object[] args )
         {
             if ( level < this.MinimumLogLevel )
                 return;
 
             this.ProcessWriteLine ( level, String.Format ( format, args ) );
         }
+
+        /// <summary>
+        /// Logs a debug message to the output
+        /// </summary>
+        /// <param name="message"></param>
+        public void LogDebug ( String message ) =>
+            this.LogMessage ( LogLevel.Debug, message );
 
         /// <summary>
         /// Logs a debug message to the output
@@ -387,6 +450,13 @@ namespace GUtils.Timing
         /// <summary>
         /// Logs an information message to the output
         /// </summary>
+        /// <param name="message"></param>
+        public void LogInformation ( String message ) =>
+            this.LogMessage ( LogLevel.Information, message );
+
+        /// <summary>
+        /// Logs an information message to the output
+        /// </summary>
         /// <param name="format"></param>
         /// <param name="args"></param>
         public void LogInformation ( String format, params Object[] args ) =>
@@ -395,10 +465,24 @@ namespace GUtils.Timing
         /// <summary>
         /// Logs a warning message to the outout
         /// </summary>
+        /// <param name="message"></param>
+        public void LogWarning ( String message ) =>
+            this.LogMessage ( LogLevel.Warning, message );
+
+        /// <summary>
+        /// Logs a warning message to the outout
+        /// </summary>
         /// <param name="format"></param>
         /// <param name="args"></param>
         public void LogWarning ( String format, params Object[] args ) =>
             this.LogMessage ( LogLevel.Warning, format, args );
+
+        /// <summary>
+        /// Logs an error message to the output
+        /// </summary>
+        /// <param name="message"></param>
+        public void LogError ( String message ) =>
+            this.LogMessage ( LogLevel.Error, message );
 
         /// <summary>
         /// Logs an error message to the output
@@ -416,15 +500,15 @@ namespace GUtils.Timing
         /// Begins a logging scope
         /// </summary>
         /// <param name="name"></param>
-        /// <param name="printElapsed">
+        /// <param name="shouldPrintElapsedTime">
         /// Whether the elapsed time on this scope should be printed at it's disposal
         /// </param>
         /// <returns></returns>
-        public IDisposable BeginScope ( String name, Boolean printElapsed )
+        public IDisposable BeginScope ( String name, Boolean shouldPrintElapsedTime = true )
         {
             this.WriteLine ( name );
             this.WriteLine ( "{" );
-            var scope = new Scope ( this, this.scopes.Count > 0 ? this.scopes.Peek ( ) : null, name, this.Elapsed, printElapsed );
+            var scope = new Scope ( this, name, this.Elapsed, shouldPrintElapsedTime );
             this.scopes.Push ( scope );
             return scope;
         }
@@ -438,11 +522,33 @@ namespace GUtils.Timing
                 throw new InvalidOperationException ( "Attempt to end outer scope without ending inner scope(s)." );
 
             scope = this.scopes.Pop ( );
-            this.WriteLine ( "}" );
             if ( scope.ShouldPrintElapsedTime )
                 this.WriteLine ( $"Elapsed {Duration.Format ( ( this.Elapsed - scope.StartedAt ).Ticks )} in {scope.Name}." );
+            this.WriteLine ( "}" );
         }
 
         #endregion Scope Management
+
+        #region Operation Management
+
+        /// <summary>
+        /// Begins an operation that won't have any logging inside it. For operations that will
+        /// oputput logs, use <see cref="BeginScope(String, Boolean)"/>.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="shouldPrintElapsedTime"></param>
+        /// <returns></returns>
+        public IDisposable BeginOperation ( String name, Boolean shouldPrintElapsedTime = true ) =>
+            new Operation ( this, name, this.Elapsed, shouldPrintElapsedTime );
+
+        private void EndOperation ( Operation operation )
+        {
+            if ( operation.ShouldPrintElapsedTime )
+                this.WriteLine ( $"Finished {operation.Name} in {Duration.Format ( ( this.Elapsed - operation.StartedAt ).Ticks )}." );
+            else
+                this.WriteLine ( $"Finished {operation.Name}." );
+        }
+
+        #endregion Operation Management
     }
 }
