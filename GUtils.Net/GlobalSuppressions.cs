7750f36f88295386eff67e0d16742e08de0b8d15
GGG KILLER <gggkiller2@gmail.com>
2020-06-16 14:13:24 -0300

    Remove unnecessary suppressions.

--- GUtils.Net/GlobalSuppressions.cs
+++ GUtils.Net/GlobalSuppressions.cs
@@ -22,5 +22,4 @@
 // suppressions either have no target or are given a specific
 // target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Usage", "CC0074:Make field readonly", Justification = "<Pending>", Scope = "member", Target = "~F:GUtils.Net.DownloadClient.UserAgent" )]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "IDE0007:Use implicit type", Justification = "<Pending>", Scope = "member", Target = "~M:GUtils.Net.DownloadClient.DownloadToStreamAsync(System.IO.Stream,System.Int32)~System.Threading.Tasks.Task" )]
