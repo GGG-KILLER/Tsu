2d65ca6abfa33fecee8b839e9c7a629ed3ce85f6
GGG KILLER <gggkiller2@gmail.com>
2020-06-16 14:18:55 -0300

    Remove unnecessary suppressions

--- GUtils.Timing/GlobalSuppressions.cs
+++ GUtils.Timing/GlobalSuppressions.cs
@@ -24,7 +24,6 @@
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Naming", "CC0029:Disposables Should Call Suppress Finalize", Justification = "This isn't a proper IDisposable implementation", Scope = "member", Target = "~M:GUtils.Timing.TimingLine.Dispose" )]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Naming", "CC0029:Disposables Should Call Suppress Finalize", Justification = "This isn't a proper IDisposable implementation", Scope = "member", Target = "~M:GUtils.Timing.TimingArea.Dispose" )]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "CC0013:Use ternary operator", Justification = "Code would get messy", Scope = "member", Target = "~M:GUtils.Timing.Timespans.Format(System.Int64,System.String)~System.String" )]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "CC0013:Use ternary operator", Justification = "Code would get messy", Scope = "member", Target = "~M:GUtils.Timing.Duration.Format(System.Int64,System.String)~System.String" )]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "IDE0046:Convert to conditional expression", Justification = "Code would get messy", Scope = "member", Target = "~M:GUtils.Timing.Duration.Format(System.Int64,System.String)~System.String" )]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Design", "CA1063:Implement IDisposable Correctly", Justification = "There is no correct implementation of IDisposable abuse", Scope = "type", Target = "~T:GUtils.Timing.TimingArea" )]
