4be9583f5497656532727361089ad2e0bd43b467
GGG KILLER <gggkiller2@gmail.com>
2020-06-16 14:10:44 -0300

    Remove unnecessary supressions.

--- GUtils.CLI/GlobalSuppressions.cs
+++ GUtils.CLI/GlobalSuppressions.cs
@@ -22,8 +22,5 @@
 // suppressions either have no target or are given a specific
 // target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "IDE0018:Inline variable declaration", Justification = "<Pending>", Scope = "member", Target = "~M:GUtils.CLI.Commands.CommandManager.Execute(System.String)" )]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "IDE0045:Convert to conditional expression", Justification = "<Pending>", Scope = "member", Target = "~M:GUtils.CLI.Commands.CommandManager.Execute(System.String)" )]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "CC0014:Use ternary operator", Justification = "<Pending>", Scope = "member", Target = "~M:GUtils.CLI.Commands.CommandManager.Execute(System.String)" )]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Design", "CA1032:Implement standard exception constructors", Justification = "<Pending>", Scope = "type", Target = "~T:GUtils.CLI.Commands.Errors.CommandDefinitionException" )]
 
