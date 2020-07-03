a9ab6f789122889c7b1444ee4af67fe3789348c9
GGG KILLER <gggkiller2@gmail.com>
2020-07-03 00:00:34 -0300

    Add GUtils.Text.Code

---
+++ GUtils.Text.Code/CodeWriter.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Text;
+
+namespace GUtils.Text.Code
+{
+    /// <summary>
+    /// A generic code writer base class.
+    /// </summary>
+    public abstract class CodeWriter
+    {
+        /// <summary>
+        /// Repeats a an <paramref name="repetitions" /> string for the number of <paramref
+        /// name="repetitions" /> provided.
+        /// </summary>
+        /// <param name="input">The string to be repeated.</param>
+        /// <param name="repetitions">
+        /// The amount of times the <paramref name="input" /> will be present on the output string.
+        /// </param>
+        /// <returns>The generated string.</returns>
+        private static String RepeatString ( String input, Int32 repetitions )
+        {
+            var builder = new StringBuilder ( input.Length * repetitions );
+            builder.Insert ( 0, input, repetitions );
+            return builder.ToString ( );
+        }
+
+        private readonly String _indentationSequence;
+        private Int32 _indentation;
+        private String _cachedIndentation;
+
+        /// <summary>
+        /// The indentation level of the writer.
+        /// </summary>
+        public Int32 Indentation
+        {
+            get => this._indentation;
+            protected set
+            {
+                if ( value < 0 )
+                    throw new ArgumentOutOfRangeException ( nameof ( value ) );
+
+                this._indentation = value;
+                this._cachedIndentation = RepeatString ( this._indentationSequence, value );
+            }
+        }
+
+        /// <summary>
+        /// Initializes this code writer.
+        /// </summary>
+        /// <param name="indentationSequence">The sequence of characters to be used as indentation.</param>
+        protected CodeWriter ( String indentationSequence )
+        {
+            this._indentationSequence = indentationSequence;
+            this.Indentation = 0;
+            this._cachedIndentation = String.Empty;
+        }
+
+        /// <summary>
+        /// Increases the indentation level.
+        /// </summary>
+        public void Indent ( ) => this.Indentation++;
+
+        /// <summary>
+        /// Decreases the indentation level.
+        /// </summary>
+        public void Outdent ( ) => this.Indentation--;
+
+        #region Write(Indented|Indentation)
+
+        /// <summary>
+        /// Writes a value to the output.
+        /// </summary>
+        /// <param name="value">The value to be written.</param>
+        public abstract void Write ( Object? value );
+
+        /// <summary>
+        /// Writes a string to the output.
+        /// </summary>
+        /// <param name="value">The string to be written to the output.</param>
+        public abstract void Write ( String? value );
+
+        /// <summary>
+        /// Writes a formatted string to the output.
+        /// </summary>
+        /// <param name="format">The format string.</param>
+        /// <param name="args">The format arguments.</param>
+        public abstract void Write ( String format, params Object?[] args );
+
+        /// <summary>
+        /// Writes the indentation prefix
+        /// </summary>
+        public void WriteIndentation ( ) =>
+            this.Write ( this._cachedIndentation );
+
+        /// <summary>
+        /// Writes a value to the output preceded by indentation.
+        /// </summary>
+        /// <param name="value">The value to be written to the output.</param>
+        public void WriteIndented ( Object? value )
+        {
+            this.WriteIndentation ( );
+            this.Write ( value );
+        }
+
+        /// <summary>
+        /// Writes a string to the output preceded by indentation.
+        /// </summary>
+        /// <param name="value">The string to be written to the output.</param>
+        public void WriteIndented ( String? value )
+        {
+            this.WriteIndentation ( );
+            this.Write ( value );
+        }
+
+        /// <summary>
+        /// Writes a formatted string to the output preceded by indentation.
+        /// </summary>
+        /// <param name="format">The format string.</param>
+        /// <param name="args">The format arguments.</param>
+        public void WriteIndented ( String format, params Object?[] args )
+        {
+            this.WriteIndentation ( );
+            this.Write ( format, args );
+        }
+
+        #endregion Write(Indented|Indentation)
+
+        #region WriteLine(Indented)
+
+        /// <summary>
+        /// Writes a line separator to the output.
+        /// </summary>
+        public abstract void WriteLine ( );
+
+        /// <summary>
+        /// Writes a value to the output followed by a line separator.
+        /// </summary>
+        /// <param name="value">The value to be written to the output.</param>
+        public abstract void WriteLine ( Object? value );
+
+        /// <summary>
+        /// Writes a string to the output followed by a line separator.
+        /// </summary>
+        /// <param name="value">The string to be written to the output.</param>
+        public abstract void WriteLine ( String? value );
+
+        /// <summary>
+        /// Writes a formatted string to the output followed by a line separator.
+        /// </summary>
+        /// <param name="format">The format string.</param>
+        /// <param name="args">The format arguments.</param>
+        public abstract void WriteLine ( String format, params Object?[] args );
+
+        /// <summary>
+        /// Writes a value to the output preceded by indentation and followed by a line separator.
+        /// </summary>
+        /// <param name="value"><inheritdoc cref="WriteLine(Object?)" /></param>
+        public void WriteLineIndented ( Object? value )
+        {
+            this.WriteIndentation ( );
+            this.WriteLine ( value );
+        }
+
+        /// <summary>
+        /// Writes a string to the output preceded by indentation and followed by a line separator.
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteLineIndented ( String? value )
+        {
+            this.WriteIndentation ( );
+            this.WriteLine ( value );
+        }
+
+        /// <summary>
+        /// Writes a formatted string to the output preceded by indentation and followed by a line separator.
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="args"></param>
+        public void WriteLineIndented ( String format, params Object?[] args )
+        {
+            this.WriteIndentation ( );
+            this.WriteLine ( format, args );
+        }
+
+        #endregion WriteLine(Indented)
+
+        /// <summary>
+        /// Increases the indentation before the callback and decreases it after
+        /// </summary>
+        /// <param name="cb"></param>
+        public void WithIndentation ( Action cb )
+        {
+            if ( cb == null )
+                throw new ArgumentNullException ( nameof ( cb ) );
+
+            this.Indent ( );
+            cb ( );
+            this.Outdent ( );
+        }
+    }
+}
---
+++ GUtils.Text.Code/StringBuilderCodeWriter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+
+namespace GUtils.Text.Code
+{
+    /// <summary>
+    /// Defines a code writer
+    /// </summary>
+    public class StringBuilderCodeWriter : CodeWriter
+    {
+        private readonly StringBuilder _builder;
+
+        /// <summary>
+        /// Initializes this class
+        /// </summary>
+        public StringBuilderCodeWriter ( String indentationSequence ) : base ( indentationSequence )
+        {
+            this._builder = new StringBuilder ( );
+        }
+
+        #region Write
+
+        /// <summary>
+        /// Writes a value
+        /// </summary>
+        /// <param name="value"></param>
+        public override void Write ( Object? value ) => this._builder.Append ( value );
+
+        /// <summary>
+        /// Writes a value
+        /// </summary>
+        /// <param name="value"></param>
+        public override void Write ( String? value ) => this._builder.Append ( value );
+
+        /// <summary>
+        /// Writes a formatted value
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="args"></param>
+        public override void Write ( String format, params Object?[] args ) => this._builder.AppendFormat ( format, args );
+
+        #endregion Write
+
+        #region WriteLine
+
+        /// <summary>
+        /// Writes an empty line
+        /// </summary>
+        public override void WriteLine ( ) => this._builder.AppendLine ( );
+
+        /// <summary>
+        /// Writes a value followed by the line terminator
+        /// </summary>
+        /// <param name="value"></param>
+        public override void WriteLine ( Object? value ) => this._builder.Append ( value ).AppendLine ( );
+
+        /// <summary>
+        /// Writes a value followed by the line terminator
+        /// </summary>
+        /// <param name="value"></param>
+        public override void WriteLine ( String? value ) => this._builder.AppendLine ( value );
+
+        /// <summary>
+        /// Writes a formatted value followed by the line terminator
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="args"></param>
+        public override void WriteLine ( String format, params Object?[] args ) => this._builder.AppendFormat ( format, args ).AppendLine ( );
+
+        #endregion WriteLine
+
+        /// <summary>
+        /// Resets the writer
+        /// </summary>
+        public void Reset ( )
+        {
+            this._builder.Clear ( );
+            this.Indentation = 0;
+        }
+
+        /// <inheritdoc />
+        public override String ToString ( ) => this._builder.ToString ( );
+    }
+}
