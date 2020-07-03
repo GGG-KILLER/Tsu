f93122472db4e9dae2eebfd701a5db816667d57b
GGG KILLER <gggkiller2@gmail.com>
2020-07-02 23:50:02 -0300

    Add Unit type to GUtils

---
+++ GUtils/Unit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GUtils
+{
+    /// <summary>
+    /// A unit value.
+    /// </summary>
+    public readonly struct Unit : IEquatable<Unit>
+    {
+        /// <summary>
+        /// The instance of the Unit type.
+        /// </summary>
+        public static readonly Unit Instance = default;
+
+        /// <inheritdoc />
+        public override Boolean Equals ( Object? obj ) => obj is Unit;
+
+        /// <inheritdoc />
+        public Boolean Equals ( Unit other ) => true;
+
+        /// <inheritdoc />
+        public override Int32 GetHashCode ( ) => -1830369473;
+
+        /// <inheritdoc />
+        public override String ToString ( ) => "()";
+    }
+}
