08be6c11134b6874c706aa52d4b7f7c1421b0c7b
GGG KILLER <gggkiller2@gmail.com>
2021-06-15 20:20:13 -0300

    Fix MVVM code style.

--- Tsu.MVVM/ViewModelBase.cs
+++ Tsu.MVVM/ViewModelBase.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿// Copyright © 2016 GGG KILLER <gggkiller2@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the “Software”), to deal in the Software without
+// restriction, including without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
@@ -21,13 +38,13 @@ namespace Tsu.MVVM
         /// <param name="field"></param>
         /// <param name="newValue"></param>
         /// <param name="propertyName"></param>
-        [MethodImpl ( MethodImplOptions.NoInlining )]
-        protected virtual void SetField<T> ( ref T field, T newValue, [CallerMemberName] String propertyName = null )
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        protected virtual void SetField<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
         {
-            if ( EqualityComparer<T>.Default.Equals ( field, newValue ) )
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
                 return;
             field = newValue;
-            this.OnPropertyChanged ( propertyName );
+            OnPropertyChanged(propertyName);
         }
 
         /// <summary>
@@ -38,10 +55,10 @@ namespace Tsu.MVVM
         /// Invoked if a <paramref name="propertyName"/> is not provided (and the value isn't
         /// auto-filled by the compiler)
         /// </exception>
-        [MethodImpl ( MethodImplOptions.NoInlining )]
-        protected virtual void OnPropertyChanged ( [CallerMemberName] String propertyName = null ) =>
-            this.PropertyChanged?.Invoke (
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(
                 this,
-                new PropertyChangedEventArgs ( propertyName ?? throw new ArgumentNullException ( nameof ( propertyName ) ) ) );
+                new PropertyChangedEventArgs(propertyName ?? throw new ArgumentNullException(nameof(propertyName))));
     }
 }
