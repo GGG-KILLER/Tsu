2df049e4ca4c6c2d0bc899a4d0b0cbbe1d75b653
GGG KILLER <gggkiller2@gmail.com>
2021-06-15 20:27:41 -0300

    Fix testing code style.

--- Tsu.Testing/DelegateInvocationCounter.cs
+++ Tsu.Testing/DelegateInvocationCounter.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿// Copyright © 2016 GGG KILLER <gggkiller2@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the “Software”), to deal in the Software without
+// restriction, including without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
 using System.Runtime.CompilerServices;
 using System.Threading;
 
@@ -11,12 +28,12 @@ namespace Tsu.Testing
     public class DelegateInvocationCounter<T>
         where T : Delegate
     {
-        private Int32 _invocationCount;
+        private int _invocationCount;
 
         /// <summary>
         /// The number of times <see cref="WrappedDelegate" /> was invoked.
         /// </summary>
-        public Int32 InvocationCount => this._invocationCount;
+        public int InvocationCount => _invocationCount;
 
         /// <summary>
         /// The wrapper delegate that increments the invocation count when invoked.
@@ -26,14 +43,14 @@ namespace Tsu.Testing
         /// <summary>
         /// Atomically increments the number of invocations of this delegate.
         /// </summary>
-        [MethodImpl ( MethodImplOptions.AggressiveInlining )]
-        internal void Increment ( ) =>
-            Interlocked.Increment ( ref this._invocationCount );
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal void Increment() =>
+            Interlocked.Increment(ref _invocationCount);
 
         /// <summary>
         /// Resets the number of invocations of this counter.
         /// </summary>
-        public void Reset ( ) =>
-            this._invocationCount = 0;
+        public void Reset() =>
+            _invocationCount = 0;
     }
 }
