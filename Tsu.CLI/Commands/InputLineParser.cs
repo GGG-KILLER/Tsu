c0c8dea1986f64c871aac4044573041220a63381
GGG KILLER <gggkiller2@gmail.com>
2021-06-15 22:02:23 -0300

    Adjust file header copyright year.

--- Tsu.CLI/Commands/InputLineParser.cs
+++ Tsu.CLI/Commands/InputLineParser.cs
@@ -1,4 +1,4 @@
-// Copyright © 2016 GGG KILLER <gggkiller2@gmail.com>
+// Copyright © 2021 GGG KILLER <gggkiller2@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software
 // and associated documentation files (the “Software”), to deal in the Software without
