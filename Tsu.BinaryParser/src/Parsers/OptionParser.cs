b9de8adbe2bf8376b2bc912451448dd1c96a6070
GGG <gggkiller2@gmail.com>
2022-08-02 09:34:06 -0300

    Include a few meta-parsers.

---
+++ Tsu.BinaryParser/src/Parsers/OptionParser.cs
@@ -0,0 +1,159 @@
+// Copyright © 2021 GGG KILLER <gggkiller2@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the “Software”), to deal in the Software without
+// restriction, including without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Tsu.BinaryParser.Parsers;
+
+namespace Tsu.BinaryParser
+{
+    /// <summary>
+    /// A wrapper parser that handles parsing of <see cref="Option{T}"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class OptionParser<T> : IBinaryParser<Option<T>>
+    {
+        private readonly IBinaryParser<T> _wrappedParser;
+        private readonly byte _noneByte;
+        private readonly byte _someByte;
+        private readonly bool _acceptEofAsNone;
+
+        /// <summary>
+        /// Initializes the builtin <see cref="Option{T}"/> parser.
+        /// </summary>
+        /// <param name="wrappedParser">
+        /// The parser that handles the <typeparamref name="T"/>.
+        /// </param>
+        /// <param name="noneByte"></param>
+        /// <param name="someByte"></param>
+        /// <param name="acceptEofAsNone"></param>
+        public OptionParser(
+            IBinaryParser<T> wrappedParser,
+            byte noneByte = 0,
+            byte someByte = 1,
+            bool acceptEofAsNone = true)
+        {
+            _wrappedParser = wrappedParser;
+            _noneByte = noneByte;
+            _someByte = someByte;
+            _acceptEofAsNone = acceptEofAsNone;
+        }
+
+        /// <inheritdoc/>
+        public Option<int> MinimumByteCount => _wrappedParser.MinimumByteCount.Map(x => x + 1);
+        /// <inheritdoc/>
+        public Option<int> MaxmiumByteCount => _wrappedParser.MaxmiumByteCount.Map(x => x + 1);
+        /// <inheritdoc/>
+        public bool IsFixedSize => _wrappedParser.IsFixedSize;
+
+        /// <inheritdoc/>
+        public Option<T> Deserialize(Stream stream, IBinaryParsingContext context)
+        {
+            var flag = stream.ReadByte();
+            if (flag is not (-1 or 0))
+            {
+                return _wrappedParser.Deserialize(stream, context);
+            }
+            return Option.None<T>();
+        }
+
+        /// <inheritdoc/>
+        public async ValueTask<Option<T>> DeserializeAsync(Stream stream, IBinaryParsingContext context, CancellationToken cancellationToken = default)
+        {
+            var flag = stream.ReadByte();
+            if (flag == _someByte)
+            {
+                return await _wrappedParser.DeserializeAsync(stream, context, cancellationToken);
+            }
+            else if (flag == _noneByte || (flag == -1 && _acceptEofAsNone))
+            {
+                return Option.None<T>();
+            }
+            else
+            {
+                throw new FormatException("First byte does not indicate either Some nor None");
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Serialize(Stream stream, IBinaryParsingContext context, Option<T> value)
+        {
+            if (value.IsNone)
+            {
+                stream.WriteByte(_noneByte);
+            }
+            else
+            {
+                stream.WriteByte(_someByte);
+                _wrappedParser.Serialize(stream, context, value.Value);
+            }
+        }
+
+        /// <inheritdoc/>
+        public ValueTask SerializeAsync(
+            Stream stream,
+            IBinaryParsingContext context,
+            Option<T> value,
+            CancellationToken cancellationToken = default)
+        {
+            if (value.IsNone)
+            {
+                stream.WriteByte(_noneByte);
+                return new ValueTask();
+            }
+            else
+            {
+                stream.WriteByte(_someByte);
+                return _wrappedParser.SerializeAsync(stream, context, value.Value, cancellationToken);
+            }
+        }
+    }
+}
+
+namespace Tsu.BinaryParser
+{
+    public static partial class BinaryParserExtensions
+    {
+        /// <summary>
+        /// Wraps the parser as an <see cref="Option{T}"/> parser.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type which is optional
+        /// </typeparam>
+        /// <param name="wrappedParser">
+        /// <inheritdoc cref="OptionParser{T}.OptionParser(IBinaryParser{T}, byte, byte, bool)" path="/param[@name='wrappedParser']"/>
+        /// </param>
+        /// <param name="noneByte">
+        /// <inheritdoc cref="OptionParser{T}.OptionParser(IBinaryParser{T}, byte, byte, bool)" path="/param[@name='noneByte']"/>
+        /// </param>
+        /// <param name="someByte">
+        /// <inheritdoc cref="OptionParser{T}.OptionParser(IBinaryParser{T}, byte, byte, bool)" path="/param[@name='someByte']"/>
+        /// </param>
+        /// <param name="acceptEofAsNone">
+        /// <inheritdoc cref="OptionParser{T}.OptionParser(IBinaryParser{T}, byte, byte, bool)" path="/param[@name='acceptEofAsNone']"/>
+        /// </param>
+        /// <returns></returns>
+        public static OptionParser<T> AsOption<T>(
+            this IBinaryParser<T> wrappedParser,
+            byte noneByte = 0,
+            byte someByte = 1,
+            bool acceptEofAsNone = true) =>
+            new OptionParser<T>(wrappedParser, noneByte, someByte, acceptEofAsNone);
+    }
+}
---
+++ Tsu.BinaryParser/src/Parsers/WithEndianessParser.cs
@@ -0,0 +1,146 @@
+// Copyright © 2021 GGG KILLER <gggkiller2@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the “Software”), to deal in the Software without
+// restriction, including without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Tsu.BinaryParser.Parsers;
+
+namespace Tsu.BinaryParser.Parsers
+{
+    /// <summary>
+    /// A meta-parser that forcefully sets the endianess for a given parser it wraps.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class WithEndianessParser<T> : IBinaryParser<T>
+    {
+        private readonly IBinaryParser<T> _wrappedParser;
+        private readonly Endianess _endianess;
+
+        /// <summary>
+        /// Initializes this meta-parser with the provided parser to be wrapped and the wanted endianess.
+        /// </summary>
+        /// <param name="wrappedParser">The parser being wrapped.</param>
+        /// <param name="endianess">The endianess to be forced.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public WithEndianessParser(IBinaryParser<T> wrappedParser!!, Endianess endianess)
+        {
+            _wrappedParser = wrappedParser;
+            _endianess = endianess;
+        }
+
+        /// <inheritdoc/>
+        public Option<int> MinimumByteCount => _wrappedParser.MinimumByteCount;
+        /// <inheritdoc/>
+        public Option<int> MaxmiumByteCount => _wrappedParser.MaxmiumByteCount;
+        /// <inheritdoc/>
+        public bool IsFixedSize => _wrappedParser.IsFixedSize;
+
+        /// <inheritdoc/>
+        public T Deserialize(Stream stream, IBinaryParsingContext context)
+        {
+            var previousEndianess = context.Endianess;
+            try
+            {
+                context.Endianess = _endianess;
+                return _wrappedParser.Deserialize(stream, context);
+            }
+            finally
+            {
+                if (context.Endianess == _endianess)
+                    context.Endianess = previousEndianess;
+            }
+        }
+
+        /// <inheritdoc/>
+        public async ValueTask<T> DeserializeAsync(Stream stream, IBinaryParsingContext context, CancellationToken cancellationToken = default)
+        {
+            var previousEndianess = context.Endianess;
+            try
+            {
+                context.Endianess = _endianess;
+                return await _wrappedParser.DeserializeAsync(stream, context, cancellationToken);
+            }
+            finally
+            {
+                if (context.Endianess == _endianess)
+                    context.Endianess = previousEndianess;
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Serialize(Stream stream, IBinaryParsingContext context, T value)
+        {
+            var previousEndianess = context.Endianess;
+            try
+            {
+                context.Endianess = _endianess;
+                _wrappedParser.Serialize(stream, context, value);
+            }
+            finally
+            {
+                if (context.Endianess == _endianess)
+                    context.Endianess = previousEndianess;
+            }
+        }
+
+        /// <inheritdoc/>
+        public async ValueTask SerializeAsync(
+            Stream stream,
+            IBinaryParsingContext context,
+            T value,
+            CancellationToken cancellationToken = default)
+        {
+            var previousEndianess = context.Endianess;
+            try
+            {
+                context.Endianess = _endianess;
+                await _wrappedParser.SerializeAsync(stream, context, value, cancellationToken);
+            }
+            finally
+            {
+                if (context.Endianess == _endianess)
+                    context.Endianess = previousEndianess;
+            }
+        }
+    }
+}
+
+namespace Tsu.BinaryParser
+{
+    public static partial class BinaryParserExtensions
+    {
+        /// <summary>
+        /// Forces a parser to use a certain endianess instead of the context's one.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="wrappedParser">
+        /// <inheritdoc cref="WithEndianessParser{T}.WithEndianessParser(IBinaryParser{T}, Endianess)" path="/param[@name='wrappedParser']"/>
+        /// </param>
+        /// <param name="endianess">
+        /// <inheritdoc cref="WithEndianessParser{T}.WithEndianessParser(IBinaryParser{T}, Endianess)" path="/param[@name='endianess']"/>
+        /// </param>
+        /// <returns></returns>
+        public static WithEndianessParser<T> WithEndianess<T>(this IBinaryParser<T> wrappedParser, Endianess endianess)
+        {
+            if (wrappedParser is WithByteOrderMark<T>)
+                throw new InvalidOperationException("A WithByteOrderMark should not be wrapped in a WithEndianessParser as that'll throw away the Byte Order Mark result.");
+            return new WithEndianessParser<T>(wrappedParser, endianess);
+        }
+    }
+}
