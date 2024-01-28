82d7488b9f01b666e2de1d026bded0a9df8560a3
GGG <gggkiller2@gmail.com>
2024-01-27 23:42:03 -0300

    Add packaging script.

---
+++ package.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p 'with pkgs.dotnetCorePackages; combinePackages [ sdk_8_0 sdk_7_0 sdk_6_0 ]'
+# shellcheck shell=bash
+set -euo pipefail
+
+rm -r packages;
+
+dotnet clean -c Debug -v quiet;
+dotnet clean -c Release -v quiet;
+
+dotnet restore;
+
+dotnet build --configuration Release --no-restore -v quiet;
+
+if ! dotnet pack --configuration Release --no-build --output packages -v quiet; then
+    echo "Failed to package."
+fi
