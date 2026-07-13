07337f18103b60dcd7ec2a7a4fa8abd179619697
GGG <github@ggg.dev>
2026-07-13 13:30:10 -0300

    Drop other Tsu libraries to focus on the main project.

--- Tsu.TreeSourceGen/sample/Generated/Tsu.TreeSourceGen/Tsu.TreeSourceGen.Generator/TreeVisitorAttribute.g.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Tsu.TreeSourceGen;
-
-[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-public sealed class TreeVisitorAttribute(Type treeRoot) : Attribute
-{
-    public Type TreeRoot { get; } = treeRoot;
-}
